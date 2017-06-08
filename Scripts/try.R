7f41a8c7f8aa41d7626ad017c0fc4272f2724320
Sotirios Damouras <sdamouras@utsc.utoronto.ca>
2017-06-08 15:37:08 -0400

    sd

--- Scripts/try.R
+++ Scripts/try.R
@@ -16,13 +16,14 @@ library("RColorBrewer")
 temp = aprogs %>% filter(sapply(Category,length)==1) %>% mutate(Category = unlist(Category)) %>%
   filter(Category %in% c("CS","MT","PT","SM","ST","SP"))
 # %>% group_by(Category) %>% summarise( Desc_all = paste(Description, collapse=" "))
-par(mfrow=c(2,3))
+par(mfrow=c(0,0), mar=c(0,0,0,0))
 for(ctg in c("CS","MT","PT","SM","ST","SP") ){
   my_word_cloud(temp %>% filter(Category==ctg) %>%.[['Description']] )  
+  #title(main=ctg)
 }
 
 
-my_word_cloud=function(x, MAX.WRDS=25){
+my_word_cloud=function(x, MAX.WRDS=15){
   docs = Corpus(VectorSource(x))
   toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
   docs <- tm_map(docs, toSpace, "/")
@@ -36,7 +37,7 @@ my_word_cloud=function(x, MAX.WRDS=25){
   docs <- tm_map(docs, removeWords, stopwords("english"))
   # Remove your own stop word
   # specify your stopwords as a character vector
-  docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+  docs <- tm_map(docs, removeWords, c("statistics", "statistical", "course", "introduction",'students',"will")) 
   # Remove punctuations
   docs <- tm_map(docs, removePunctuation)
   # Eliminate extra white spaces
